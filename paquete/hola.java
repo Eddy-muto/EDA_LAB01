93e768923ce3b6ed5c1d6b64a069d9343720bbde
eddy muto montesinos <eddy_oficiles@hotmail.com>
2023-07-26 21:03:50 -0500

    agregue mas ejemplos

--- paquete/hola.java
+++ paquete/hola.java
@@ -7,5 +7,9 @@ public class hola {
         System.out.println("hola mundo .java");
         persona p1= new persona("jose", 12);
         System.out.println(p1.toString());
+        persona p2= new persona("maria", 12);
+        System.out.println(p1.toString());
+        persona p3= new persona("juan", 12);
+        System.out.println(p1.toString());
     }
 }
